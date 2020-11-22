f00ea252ea5304abfd1eb08ffa37cf641cc79446
Miguel Peña <miguel.pena@sarria.salesians.cat>
2020-11-22 13:05:45 +0100

    Añadidos efectos a la black arrow de la page1 y al botón de la page2 de la ScrollView

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:animate_do/animate_do.dart';
 
 class ScrollPage extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        scrollDirection: Axis.vertical,
-        children: <Widget>[
-          _pagina1(),
-          _pagina2(context)
-        ],
-      )
-    );
+        body: PageView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[_pagina1(), _pagina2(context)],
+    ));
   }
 
   Widget _pagina1() {
@@ -26,19 +21,15 @@ class ScrollPage extends StatelessWidget {
     );
   }
 
-
   Widget _colorFondo() {
-
     return Container(
       width: double.infinity,
       height: double.infinity,
       color: Color.fromRGBO(108, 192, 218, 1.0),
     );
-
   }
 
   Widget _imagenFondo() {
-
     return Container(
       width: double.infinity,
       height: double.infinity,
@@ -47,25 +38,25 @@ class ScrollPage extends StatelessWidget {
         fit: BoxFit.cover,
       ),
     );
-
   }
 
   Widget _textos() {
-
     final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
 
     return SafeArea(
       child: Column(
         children: <Widget>[
-          SizedBox(height: 20.0 ),
-          Text('You missed', style: estiloTexto ),
-          Text('A SEMICOLON', style: estiloTexto ),
-          Expanded( child: Container() ),
-          Icon( Icons.keyboard_arrow_down, size: 150.0, color: Colors.black54 )
+          SizedBox(height: 20.0),
+          Text('You missed', style: estiloTexto),
+          Text('A SEMICOLON', style: estiloTexto),
+          Expanded(child: Container(height: 30,)),
+          Bounce(
+              infinite: true,
+              child: Icon(Icons.keyboard_arrow_down,
+                  size: 150.0, color: Colors.black54))
         ],
       ),
     );
-
   }
 
   Widget _pagina2(BuildContext context) {
@@ -77,29 +68,28 @@ class ScrollPage extends StatelessWidget {
               begin: FractionalOffset(0.0, 0.3),
               end: FractionalOffset(0.0, 1.0),
               colors: [
-                Colors.yellow,
-                Colors.orange,
-                Colors.red,
-                Colors.red[900]
-              ]
-          )
-      ),
+            Colors.yellow,
+            Colors.orange,
+            Colors.red,
+            Colors.red[900]
+          ])),
       child: Center(
-        child: RaisedButton(
-          elevation: 10,
-          shape: StadiumBorder(),
-          color: Colors.red[900],
-          textColor: Colors.white,
-
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-            child: Text('Welcome to Debug Hell', style: TextStyle(fontSize: 25.0)),
+        child: ElasticIn(
+          duration: Duration(milliseconds: 2000),
+          child: RaisedButton(
+            elevation: 10,
+            shape: StadiumBorder(),
+            color: Colors.red[900],
+            textColor: Colors.white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+              child:
+                  Text('Welcome to Debug Hell', style: TextStyle(fontSize: 25.0)),
+            ),
+            onPressed: () => Navigator.pushNamed(context, 'botones'),
           ),
-          onPressed: ()=> Navigator.pushNamed(context, 'botones'),
         ),
       ),
     );
   }
-
-
 }
